bbb449572117fea9aaf4cc662311466a15981da1
sondrehav <sondre.havellen@gmail.com>
2015-03-18 11:09:05 +0100

    WMLMLWML

--- src/Client.py
+++ src/Client.py
@@ -1,6 +1,10 @@
-import socket, threading, sys
+import socket, threading, sys, time
 
-HOST = input('IP \t> ')
+def printMsg(inputMsg, name):
+    print('['+str(time.time())+'; '+str(name)+']:\t'+str(inputMsg))
+
+HOST = input('[IP]:\t')
+printMsg(HOST, 'CON')
 PORT = 6773
 BUFSIZE = 1024
 ADDR = (HOST, PORT)
@@ -8,20 +12,22 @@ ADDR = (HOST, PORT)
 clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 clientSocket.connect(ADDR)
 
-login = input('SEND > ')
-clientSocket.send(login)
+login = input('[NAME]:\t')
+clientSocket.send(login.encode('ascii'))
+dt = clientSocket.recv(BUFSIZE)
+printMsg(dt, 'RECV')
 
 def recieveFunc():
     while(True):
         message = clientSocket.recv(BUFSIZE)
         if not message: sys.exit()
-        print('MSG \t> ' + message)
+        print(printMsg(message, 'RECV'))
 
 threading.Thread(target=recieveFunc).start()
 
 while(True):
-    inputMsg = input('> ')
+    inputMsg = input('[SEND]:\t')
     if not inputMsg: break
-    clientSocket.send(inputMsg)
+    clientSocket.send(inputMsg.encode('ascii'))
     
 clientSocket.close()
--- src/Server.py
+++ src/Server.py
@@ -1,30 +1,31 @@
 import socket, threading, time, sys
 
-HOST = "localhost"
+def printMsg(inputMsg, name):
+    print('['+str(time.time())+'; '+str(name)+']:\t'+str(inputMsg))
+    
+HOST = 'localhost'
 PORT = 6773
 BUFSIZE = 1024
 ADDR = (HOST, PORT)
 SOCKETS = []
 
-print "Starting server on (" + str(HOST) + ", " + str(PORT) + ")" + "..."
+printMsg('Starting server on ' + str(HOST) + ':' + str(PORT) + '.', 'START')
 
 serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 serverSocket.bind(ADDR)
-serverSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+#serverSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 serverSocket.listen(1)
 
 SOCKETS.append(serverSocket)
 
-print "Server bound on " + str(socket.gethostname())
-
-
+printMsg('Successful.', 'START')
 
 def recieveFunc():
     conn, addr = serverSocket.accept()
-    print 'hello ' + str(addr)
+    printMsg('Connection from ' + str(addr)), 'CON')
     while(True):
         message = serverSocket.recv(BUFSIZE)
-        print "[" + str(time.time()) + "]: " + message
+        printMsg(message, 'MSG')
     return
 
 threading.Thread(target=recieveFunc).start()
@@ -33,6 +34,6 @@ while(True):
     data = input()
     if not data: break;
 
-print "Closing server..."
+printMsg('Closing...', 'STOP')
 
 serverSocket.close()
